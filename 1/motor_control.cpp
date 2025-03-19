9a7a60a58258b7451f73a867c70fd497e1bc4838
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-03-20 00:58:38 +0530

    Update motor_control.cpp

--- 1/motor_control.cpp
+++ 1/motor_control.cpp
@@ -1,1 +1,28 @@
 #include "motor_control.h"
+#define LRPWM 9
+#define LLPWM 10
+#define LPWM 11
+#define RRPWM 7
+#define RLPWM 8
+#define RPWM 9
+
+void setupMotors() {
+ 
+pinMode(LRPWM, OUTPUT);
+pinMode(LLPWM, OUTPUT);
+pinMode(LPWM, OUTPUT);
+pinMode(RRPWM, OUTPUT);
+pinMode(RLPWM, OUTPUT);
+pinMode(RPWM, OUTPUT);
+     
+}
+
+void moveForward() {
+  digitalWrite(LRPWM, HIGH);
+  digitalWrite(LLPWM, LOW);
+  analogWrite(LPWM, 255);
+  digitalWrite(RRPWM, HIGH);
+  digitalWrite(RLPWM, LOW);
+  analogWrite(RPWM, 255);
+ 
+}
