68d3a3d623bbc99e33b9ad1111c3f6d9c86fc977
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-03-20 00:58:45 +0530

    Update motor_control.h

---
+++ 1/motor_control.h
@@ -0,0 +1,13 @@
+#ifndef MOTOR_CONTROL_H
+#define MOTOR_CONTROL_H
+
+#include <Arduino.h>
+
+void setupMotors();
+void moveForward();
+//void moveBackward();
+//void turnLeft();
+//void turnRight();
+//void stopMotors();
+
+#endif
