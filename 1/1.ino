82abb9b316abcf5414f7ab3b33e9f75118264cfd
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-03-21 01:29:52 +0530

    Update 1.ino

--- 1/1.ino
+++ 1/1.ino
@@ -1,19 +1,25 @@
 #include "motor_control.h"
-#include"sensor_processing.h"
+#include "sensor_processing.h"
 
 void setup() {
-Serial.begin(115200); 
-setupMotors();
-
+    Serial.begin(9600); 
+    setupMotors();
+    setupSensors();  // Initialize sensors
 }
 
 void loop() {
-sendIRSensorData();
-moveForward();
-delay(3000);
-stopMotors();
-delay(1000);
-turnRight();
-delay(1000);
+    sendIRSensorData();  
 
+    // Move forward until both IR sensors detect no object (0)
+    while (digitalRead(IR_FL) == 1 && digitalRead(IR_FR) == 1) {
+        moveForward();
+        sendIRSensorData();  // Continuously check IR sensor values
+    }
+
+    stopMotors();  // Stop when IR sensors detect something
+    delay(1000);   // Pause for 1 second
+
+    turnRight();   // Turn right after stopping
+    delay(1000);   // Delay for turn completion
 }
+
