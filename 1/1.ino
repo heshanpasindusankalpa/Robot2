51707068d53fd9028aa63223e23306ad35f82d77
heshanpasindusankalpa <pasindusankalpa2021@gmail.com>
2025-03-21 15:51:58 +0530

    Update 1.ino

--- 1/1.ino
+++ 1/1.ino
@@ -3,7 +3,7 @@
 
 #define BUZZER_PIN 12  // Define buzzer pin
 
-int zeroCount = 0; // Counter for (0,0,0) readings
+int zeroCount = 0; // Counter for (0,0) readings
 
 void setup() {
     Serial.begin(9600); 
@@ -15,32 +15,33 @@ void setup() {
 void loop() {
     sendIRSensorData();
 
-    // Move forward until all IR sensors detect no object (0,0,0)
-    while (digitalRead(IR_FR) == 1 && digitalRead(IR_FL) == 1 && digitalRead(IR_FM) == 1) {
+    // Move forward until both IR sensors detect no object (0,0)
+    while ( digitalRead(IR_FR) == 1 &&  digitalRead(IR_FL) == 1 &&  digitalRead(IR_FM)==1) {
         moveForward();
         sendIRSensorData();  // Continuously check IR sensor values
     }
 
-    stopMotors();  // Stop when IR sensors detect (0,0,0)
+    stopMotors();  // Stop when IR sensors detect (0,0)
     delay(1000);   // Pause for 1 second
 
     digitalWrite(BUZZER_PIN, HIGH);  // Turn on buzzer
     delay(500);  // Buzzer on for 0.5 seconds
     digitalWrite(BUZZER_PIN, LOW);   // Turn off buzzer
 
-    zeroCount++; // Increment count when (0,0,0) is detected
+    zeroCount++; // Increment count when (0,0) is detected
 
     if (zeroCount == 1) {  
-        // First time seeing (0,0,0), just move forward
+        // First time seeing (0,0), just move forward
         moveForward();
         delay(1000);
     } 
     else if (zeroCount == 2) {  
-        // Second time seeing (0,0,0), turn right
+        // Second time seeing (0,0), turn right
         turnRight();
         delay(1000);
+        zeroCount = 0; // Reset counter after turning
     }
-    else if (zeroCount == 3) {  
+     else if (zeroCount == 3) {  
         // Third time seeing (0,0,0), stop completely
         stopMotors();
         Serial.println("Stopping completely after 3 detections!");
